0b1e61c3e50ad35491f4aaef7a77ad4b11bf3343
Fabio <fabiomanuelsilvamarques@gmail.com>
2021-04-30 15:43:05 +0200

    modifié pour charger les configs depuis le config manager

--- Labo_SMTP/src/main/java/Main.java
+++ Labo_SMTP/src/main/java/Main.java
@@ -1,12 +1,28 @@
 import config.ConfigurationManager;
+import prank.Prank;
+import prank.PrankGenerator;
+import smtp.SmtpClient;
+
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] messages = ConfigurationManager.getMessages();
 
-        for (String message :
-                messages) {
-            System.out.println(message);
+    public static void main(String[] args){
+        ConfigurationManager configurationManager;
+
+
+        configurationManager = new ConfigurationManager();
+        String serverAddress = configurationManager.getPropValue("smtpServerAddress");
+        int serverPort = Integer.parseInt(configurationManager.getPropValue("smtpServerPort"));
+
+        SmtpClient client = new SmtpClient(serverAddress, serverPort);
+
+        PrankGenerator prankGenerator = new PrankGenerator(configurationManager);
+        ArrayList<Prank> pranks = prankGenerator.createAllPranks();
+
+        for (Prank prank : pranks) {
+            client.send( prank.createMail());
         }
     }
+
 }
